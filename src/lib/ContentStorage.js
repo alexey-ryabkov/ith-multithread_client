f2db0412e5db99dd1d12f8963b46ea9e4765e965
alexey-ryabkov <xelaris@ya.ru>
2024-06-25 22:32:07 +0300

    ft: (client) add func for storage content

---
+++ src/lib/ContentStorage.js
@@ -0,0 +1,65 @@
+import { blobToBase64 } from './utils';
+
+export default class ContentStorage {
+	/** @type {ContentStorage} */
+	static _instance;	
+	_keys = [];
+
+	constructor() {
+		if (ContentStorage._instance) {
+			throw new Error("Use ContentStorage.instance for use ContentStorage singleton class object");
+		}
+		this._storage = window.localStorage;
+		for (let i = 0; i < this._storage.length; i++) {
+			this._keys.push(this._storage.key(i));
+		}
+		ContentStorage._instance = this;
+	}
+	/**
+	 * @returns {ContentStorage}
+	 */
+	static get instance() {
+		if (!ContentStorage._instance) {
+      ContentStorage._instance = new ContentStorage();
+    }
+		return ContentStorage._instance;		
+	}
+	/**
+	 * @returns {string[]}
+	 */
+	get keys() {
+		return this._keys;
+	}
+	/**
+	 * @param {string} key
+	 * @param {Blob|string} content
+	 */
+	async save(key, content) {
+		if (typeof content !== 'string') {
+      content = await blobToBase64(content);
+		}
+    this._storage.setItem(key, /** @type {string} */ (content));
+		this._keys.push(key);
+	}
+	/**
+	 * @param {string} key
+	 * @returns {string|null}
+	 */
+	async get(key) { 
+    return this._storage.getItem(key);
+  }
+	/**
+	 * @param {string} key
+	 * @returns {Object.<string,string>}
+	 */
+	getAll() {
+    return this._keys.reduce(async (allContent, key) => {
+      allContent[key] = this.get(key);
+      return allContent;
+    }, {});
+  }
+
+	_clear() {
+		this._storage.clear();
+	}
+}
--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -1,24 +1,52 @@
 /**
- * @param {number} bytes 
- * @param {number} decimals 
+ * @param {number} bytes
+ * @param {number} decimals
  * @returns {string}
  */
 export function formatBytes(bytes, decimals = 2) {
-  if (bytes === 0) return '0 bytes';
+	if (bytes === 0) return '0 bytes';
 
-  const k = 1024;
-  const dm = decimals < 0 ? 0 : decimals;
-  const sizes = ['bytes', 'Kb', 'Mb', 'Gb', 'Tb', 'Pb', 'Eb', 'Zb', 'Yb'];
+	const k = 1024;
+	const dm = decimals < 0 ? 0 : decimals;
+	const sizes = ['bytes', 'Kb', 'Mb', 'Gb', 'Tb', 'Pb', 'Eb', 'Zb', 'Yb'];
 
-  const i = Math.floor(Math.log(bytes) / Math.log(k));
-  let formated = parseFloat((bytes / Math.pow(k, i)).toFixed(dm));
-  let measure = sizes[i];
+	const i = Math.floor(Math.log(bytes) / Math.log(k));
+	let formated = parseFloat((bytes / Math.pow(k, i)).toFixed(dm));
+	let measure = sizes[i];
 
-  if (isNaN(formated)) {
-    formated = 'unknown';
-  }
-  if (!measure || !formated) {
-    measure = '';
-  }
-  return `${formated}${measure}`;
+	if (isNaN(formated)) {
+		formated = 'unknown';
+	}
+	if (!measure || !formated) {
+		measure = '';
+	}
+	return `${formated}${measure}`;
+}
+/**
+ * @param {Blob} blob 
+ * @returns 
+ */
+export async function blobToBase64(blob) {
+  return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onloadend = () => resolve(reader.result);
+      reader.onerror = reject;
+      reader.readAsDataURL(blob);
+  });
+}
+/**
+ * 
+ * @param {string} base64 
+ * @param {string} type 
+ * @returns Blob
+ */
+export function base64ToBlob(base64, type) {
+	const byteCharacters = atob(base64.split(',')[1]);
+	const byteNumbers = new Array(byteCharacters.length);
+  
+	for (let i = 0; i < byteCharacters.length; i++) {
+		byteNumbers[i] = byteCharacters.charCodeAt(i);
+	}
+	const byteArray = new Uint8Array(byteNumbers);
+	return new Blob([byteArray], { type });
 }

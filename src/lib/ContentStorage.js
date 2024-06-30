a8878651e1305b388019544b2bba70edc6804fd8
alexey-ryabkov <xelaris@ya.ru>
2024-06-30 11:09:28 +0300

    ft: complete ContentStorage

--- src/lib/ContentStorage.js
+++ src/lib/ContentStorage.js
@@ -1,17 +1,29 @@
-import { blobToBase64 } from './utils';
+import { blobToBase64, deepMerge } from './utils';
+
+/**
+ * @typedef {string} Key
+ * @typedef {string} Group
+ * @typedef {string} StoredItem
+ * @typedef {Object.<Key,StoredItem>} StoredItems
+ * @typedef {Object<Group,Key[]>} Meta
+ */
 
 export default class ContentStorage {
+	static _META_KEY = '__meta__';
 	/** @type {ContentStorage} */
-	static _instance;	
+	static _instance;
+	/** @type {Key[]} */
 	_keys = [];
 
 	constructor() {
 		if (ContentStorage._instance) {
-			throw new Error("Use ContentStorage.instance for use ContentStorage singleton class object");
+			throw new Error('Use ContentStorage.instance for use ContentStorage singleton class object');
 		}
-		this._storage = window.localStorage;
-		for (let i = 0; i < this._storage.length; i++) {
-			this._keys.push(this._storage.key(i));
+		for (let i = 0; i < localStorage.length; i++) {
+			const key = localStorage.key(i);
+			if (key !== ContentStorage._META_KEY) {
+				this._keys.push(localStorage.key(i));
+			}
 		}
 		ContentStorage._instance = this;
 	}
@@ -20,46 +32,94 @@ export default class ContentStorage {
 	 */
 	static get instance() {
 		if (!ContentStorage._instance) {
-      ContentStorage._instance = new ContentStorage();
-    }
-		return ContentStorage._instance;		
+			ContentStorage._instance = new ContentStorage();
+		}
+		return ContentStorage._instance;
 	}
 	/**
-	 * @returns {string[]}
+	 * @returns {Key[]}
 	 */
 	get keys() {
 		return this._keys;
 	}
 	/**
-	 * @param {string} key
-	 * @param {Blob|string} content
+	 * @returns {Object.<Group,Key[]>}
 	 */
-	async save(key, content) {
-		if (typeof content !== 'string') {
-      content = await blobToBase64(content);
-		}
-    this._storage.setItem(key, /** @type {string} */ (content));
-		this._keys.push(key);
+	get keysByGroups() {
+		return this._meta;
 	}
 	/**
-	 * @param {string} key
-	 * @returns {string|null}
+	 * @returns {Object.<Group,StoredItems>}
 	 */
-	get(key) { 
-    return this._storage.getItem(key);
-  }
+	get itemsByGroups() {
+		const content = this.getItems();
+		return Object.entries(this._meta).reduce((allRes, [group, keys]) => {
+			allRes[group] = keys.reduce((groupRes, key) => {
+				groupRes[key] = content[key];
+				return groupRes;
+			}, {});
+			return allRes;
+		}, {});
+	}
 	/**
-	 * @param {string} key
-	 * @returns {Object.<string,string>}
+	 * @param {Key} key
+	 * @returns {StoredItem|null}
 	 */
-	getAll() {
-    return this._keys.reduce(async (allContent, key) => {
-      allContent[key] = this.get(key);
-      return allContent;
-    }, {});
-  }
-
-	_clear() {
-		this._storage.clear();
+	getItem(key) {
+		return localStorage.getItem(key);
+	}
+	/**
+	 * @param {Key[]} [filter]
+	 * @returns {StoredItems}
+	 */
+	getItems(filter) {
+		return this._keys
+			.filter((key) => !filter || filter.includes(key))
+			.reduce(async (allContent, key) => {
+				allContent[key] = this.getItem(key);
+				return allContent;
+			}, {});
+	}
+	/**
+	 * @param {Group} group
+	 * @returns {StoredItems}
+	 */
+	getItemsInGroup(group) {
+		return this.getItems(this._meta?.[group]);
+	}
+	/**
+	 * @param {Group} group
+	 * @returns {Object.<Group,Key[]>}
+	 */
+	getKeysInGroup(group) {
+		return this._meta?.[group] ?? [];
+	}
+	/**
+	 * @param {Key} key
+	 * @param {Blob|string} content
+	 * @param {Group} [group]
+	 */
+	async save(key, content, group = 'uncategorized') {
+		if (typeof content !== 'string') {
+			content = await blobToBase64(content);
+		}
+		localStorage.setItem(key, /** @type {string} */ (content));
+		this._keys.push(key);
+		this._addMeta({ [group]: [key] });
+	}
+	clear() {
+		localStorage.clear();
+	}
+	/**
+	 * @returns {Meta}
+	 */
+	get _meta() {
+		return JSON.parse(localStorage.getItem(ContentStorage._META_KEY) ?? '{}');
+	}
+	/**
+	 * @param {Meta} meta
+	 */
+	_addMeta(meta) {
+		localStorage.setItem(ContentStorage._META_KEY, JSON.stringify(deepMerge(this._meta, meta)));
 	}
 }
--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -23,30 +23,59 @@ export function formatBytes(bytes, decimals = 2) {
 	return `${formated}${measure}`;
 }
 /**
- * @param {Blob} blob 
- * @returns 
+ * @param {Blob} blob
+ * @returns
  */
 export async function blobToBase64(blob) {
-  return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.onloadend = () => resolve(reader.result);
-      reader.onerror = reject;
-      reader.readAsDataURL(blob);
-  });
+	return new Promise((resolve, reject) => {
+		const reader = new FileReader();
+		reader.onloadend = () => resolve(reader.result);
+		reader.onerror = reject;
+		reader.readAsDataURL(blob);
+	});
 }
 /**
- * 
- * @param {string} base64 
- * @param {string} type 
+ * @param {string} base64
+ * @param {string} type
  * @returns Blob
  */
 export function base64ToBlob(base64, type) {
 	const byteCharacters = atob(base64.split(',')[1]);
 	const byteNumbers = new Array(byteCharacters.length);
-  
+
 	for (let i = 0; i < byteCharacters.length; i++) {
 		byteNumbers[i] = byteCharacters.charCodeAt(i);
 	}
 	const byteArray = new Uint8Array(byteNumbers);
 	return new Blob([byteArray], { type });
+}
+/**
+ * @param {unknown} obj
+ * @returns {boolean}
+ */
+function isObject(obj) {
+	return obj && typeof obj === 'object' && !Array.isArray(obj);
+}
+/**
+ * @param {Object} target
+ * @param {Object} source
+ * @returns {Object}
+ */
+export function deepMerge(target, ...sources) {
+	if (!sources.length) return target;
+	const source = sources.shift();
+	if (isObject(target) && isObject(source)) {
+		for (const key in source) {
+			if (isObject(source[key])) {
+				if (!target[key]) Object.assign(target, { [key]: {} });
+				deepMerge(target[key], source[key]);
+			} else if (Array.isArray(source[key])) {
+				if (!target[key]) Object.assign(target, { [key]: [] });
+				target[key] = Array.from(new Set([...target[key], ...source[key]]));
+			} else {
+				Object.assign(target, { [key]: source[key] });
+			}
+		}
+	}
+	return deepMerge(target, ...sources);
 }
